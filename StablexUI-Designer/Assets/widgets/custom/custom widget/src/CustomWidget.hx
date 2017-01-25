5ff9b2b8385a0ffaf9351b0b6cae56b3066aef7b
r3d9u11 <r3d9u11@gmail.com>
2017-01-25 17:10:12 +0300

    CustomWidget fixes

--- StablexUI-Designer/Assets/widgets/custom/custom widget/src/CustomWidget.hx
+++ StablexUI-Designer/Assets/widgets/custom/custom widget/src/CustomWidget.hx
@@ -7,7 +7,8 @@ import ru.stablex.ui.widgets.*;
 
 class CustomWidget extends Widget {
 	public function new () {
-		trace('Wgt costuctor');
+		trace("CustomWidget:new");
+		
 		super();
 		
 		this.w = 200;
@@ -15,49 +16,67 @@ class CustomWidget extends Widget {
 		
 		this.top = 10;
 		this.left = 10;
-		
-		var skin:Paint = new Paint();
-		skin.color = 0xC0C0C0;
-		skin.border = 1;
-		skin.borderColor = 0xFF00FF;
-		
-		this.skin = skin;
 	}
 	
-	/*override public function onCreate() : Void {
-		trace('Wgt onCreate');
+	override public function onCreate() : Void {
+		trace("CustomWidget:onCreate");
+		
 		super.onCreate();
 	}
+	
 	override public function free (recursive:Bool = true) : Void {
-		trace('Wgt free');
+		trace("CustomWidget:free");
+		
 		super.free(recursive);
-	}*/
+	}
+	
 	override public function applySkin () : Void {
-		//var d = null;
-		//d.qwe();
-		//trace('Wgt applySkin');
-		trace(this.initialized);
-		trace(this.skin != null);
+		trace("CustomWidget:applySkin");
+		
 		super.applySkin();
 	}
-	/*override public function applyLayout () : Void {
-		trace('Wgt applyLayout');
+	
+	override public function applyLayout () : Void {
+		trace("CustomWidget:applyLayout");
+		
 		super.applyLayout();
 	}
+	
 	override public function refresh() : Void {
-		trace('Wgt refresh');
+		trace("CustomWidget:refresh");
+		
 		super.refresh();
 	}
+	
 	override public function onNewParent(newParent:Widget) : Void {
-		trace('Wgt onNewParent');
+		trace("CustomWidget:onNewParent");
+		
 		super.onNewParent(newParent);
 	}
+	
 	override public function resize(width:Float, height:Float, keepPercentage:Bool = false) : Void {
-		trace('Wgt resize');
+		trace("CustomWidget:resize");
+		trace('width: $width');
+		trace('height: $height');
+		trace('keepPercentage: $keepPercentage');
+		
 		super.resize(width, height, keepPercentage);
 	}
+	
 	override public function onResize() : Void {
-		trace('Wgt onResize');
+		trace("CustomWidget:onResize");
+		
 		super.onResize();
-	}*/
+	}
+	
+	override private function _onParentResize(e:WidgetEvent) : Void {
+		trace("CustomWidget:onParentResize");
+		trace('target: ${Type.getClassName(Type.getClass(e.currentTarget))}');
+		
+		super.applySkin();
+		
+		//...
+		//...
+		//...
+	}
 }
--- StablexUI-Designer/Source/rn/stablex/designer/SuidJson.hx
+++ StablexUI-Designer/Source/rn/stablex/designer/SuidJson.hx
@@ -6,11 +6,7 @@ class SuidJson {
 	
 	public static function encode (jsonStruct:Dynamic) : String
 		return tjson.TJSON.encode(jsonStruct);
-		//return haxe.Json.stringify(jsonStruct);
-		//return haxe.format.JsonParser.parse(jsonStruct);
 	
 	public static function parse (jsonStr:String) : Dynamic
 		return tjson.TJSON.parse(jsonStr);
-		//return haxe.Json.parse(jsonStr);
-		//return haxe.format.JsonPrinter.print(jsonStr);
 }
